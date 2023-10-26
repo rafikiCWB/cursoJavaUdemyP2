67d673895a09f9e5347b24210ae8acdc4738586e
rafael <sta_rafinha@hotmail.com>
2023-10-26 01:41:58 -0300

    Add SymmetricOrPalindromo class
    A new file, SymmetricOrPalindromo.java, has been added to the org.example.treiniprojectjava package. This class contains a method to check whether a given string is a palindrome, and a main method that takes user input and prints either "1" if the input is a palindrome, or a random number if it's not.

---
+++ src/main/java/org/example/treiniprojectjava/SymmetricOrPalindromo.java
@@ -0,0 +1,20 @@
+package org.example.treiniprojectjava;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class SymmetricOrPalindromo {
+    public static void main(String[] args) {
+        var teclado = new Scanner(System.in);
+        var palindromo = teclado.nextLine();
+        var random = new Random();
+        var numberRandom = random.nextInt(50);
+        System.out.println(isSimmetric(palindromo) ? "1" : numberRandom);
+    }
+    public static boolean isSimmetric(String a) {
+       /// String str = Integer.toString(a); // Essa linha de código converte um valor inteiro em uma String
+        String reverseStr = new StringBuilder(a)
+                .reverse().toString();
+        return (a.equals(reverseStr));
+    }
+}
