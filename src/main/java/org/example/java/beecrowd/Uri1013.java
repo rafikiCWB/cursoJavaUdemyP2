dfa049638676e4d9ac313cb627a9b2ee8d693c68
rafinha <sta_rafinha@hotmail.com>
2024-06-04 16:03:23 -0300

    feat: show the largest of three numbers

---
+++ src/main/java/org/example/java/beecrowd/Uri1013.java
@@ -0,0 +1,20 @@
+package org.example.java.beecrowd;
+
+import java.util.Scanner;
+
+public class Uri1013 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        var a = sc.nextInt();
+        var b = sc.nextInt();
+        var c = sc.nextInt();
+
+        var maiorAB  = (a + b + Math.abs(a - b)) / 2;
+        var maiorABC = (maiorAB + c + Math.abs(maiorAB - c)) /2;
+        System.out.println(maiorABC);
+
+
+        sc.close();
+    }
+}
