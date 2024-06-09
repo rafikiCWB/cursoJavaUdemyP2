3ff18e144743dcfa4be270508ed5a32945c94946
rafinha <sta_rafinha@hotmail.com>
2024-06-08 23:50:37 -0300

    Feat: add some function about String

--- src/main/java/org/example/logica/FunctionSplit.java
+++ src/main/java/org/example/logica/FunctionSplit.java
@@ -3,7 +3,7 @@ package org.example.logica;
 import java.util.Arrays;
 
 /*
-* A função split divide uma String em uma substring
+ * A função split divide uma String em uma substring
  */
 public class FunctionSplit {
     public static void main(String[] args) {
@@ -15,5 +15,16 @@ public class FunctionSplit {
         String[] result = b.split("[^oa]"); // Aqui estou usando regex para imprimir a String exceto a letra 'o' e leta 'a'
         System.out.println(Arrays.toString(result));
 
+        StringBuilder strb = new StringBuilder();
+        strb.append("Hello World!");
+        System.out.println(strb);
+
+        strb.insert(5, " Java ");
+        System.out.println(strb);
+
+        String nome = "Rafael";
+        int idade = 27;
+        var resultado = String.format("%s tem %d anos", nome, idade);
+        System.out.println(resultado);
     }
 }
